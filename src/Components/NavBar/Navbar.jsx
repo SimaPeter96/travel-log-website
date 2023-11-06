2ec2b762bcf2445fb428fba0945fca265f1b76f8
Sima Peter <122967906+SimaPeter96@users.noreply.github.com>
2023-11-06 14:55:44 +0200

    added two Images in image folder

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React from "react"
 import "./Navbar.css";
+import {TravelLogo} from '../Images/TravelAppLogo.png'
 
-const Navbar = () => {
-    return <div>
-        Navbar
+const Navbar = ()  => {
+    return <div className="Navbar">
+        <div className="LogoDiv">
+          <TravelLogo className='logo'/>  
+        </div>
     </div>;
-};
+}
+
+
 
 export default Navbar;
